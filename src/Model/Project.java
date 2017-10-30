d8522c59403c5699f74129f7a4476df0c80e3e3b
dearvae <lovevae7@gmail.com>
2017-10-30 13:52:56 +0800

    Update Project.java

--- src/Model/Project.java
+++ src/Model/Project.java
@@ -5,15 +5,68 @@
  */
 package Model;
 
+import Enum.Gender;
+import java.util.ArrayList;
+
 /**
  *
  * @author BN
  */
 public class Project {
-    Student[] student;
-    String projectTittle;
-    String school;
-    String supervisorName;
+    private String projectTitle;
+    private String school;
+    private String supervisorName;
+    private int numOfStudents;
+    private ArrayList<Student> students = new ArrayList<Student>();
+    
+    public Project(String projectTitle, String school, String supervisorName, int numOfStudents){
+        this.projectTitle = projectTitle;
+        this.school = school;
+        this.supervisorName = supervisorName;
+        this.numOfStudents = numOfStudents;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public void setProjectTitle(String projectTitle) {
+        this.projectTitle = projectTitle;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getSupervisorName() {
+        return supervisorName;
+    }
+
+    public void setSupervisorName(String supervisorName) {
+        this.supervisorName = supervisorName;
+    }
+
+    public int getNumOfStudents() {
+        return numOfStudents;
+    }
+
+    public void setNumOfStudents(int numOfStudents) {
+        this.numOfStudents = numOfStudents;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    
+    public void addStudent(String admissionNo, String name, String course, Gender gender){
+        Student student = new Student(admissionNo, name, course, gender);
+        students.add(student);        
+    }
     
     
 }
