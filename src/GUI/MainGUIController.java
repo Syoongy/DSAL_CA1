1591f0af89958bf64cb8c269486fdffe8c7bb272
Fan Syong Yue <Fan Syong Yue@mastersyong>
2017-11-21 00:59:40 +0800

    Completed redesign of GUI - Edit mode working

--- src/GUI/MainGUIController.java
+++ src/GUI/MainGUIController.java
@@ -11,13 +11,17 @@ import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -26,7 +30,8 @@ import javafx.stage.Stage;
 public class MainGUIController implements Initializable {
 
     ObservableList<String> genderList = FXCollections.observableArrayList("Male", "Female");
-
+    private double xOffset = 0, yOffset = 0;
+    
     @FXML
     private ComboBox genderBox;
     @FXML
@@ -53,6 +58,25 @@ public class MainGUIController implements Initializable {
 
     public void closeApp(MouseEvent evt) {
         Platform.exit();
+    }
+
+    public void addProject(MouseEvent evt) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AddProject.fxml"));
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+        Scene scene = new Scene(root);
+        //Platform.setImplicitExit(false);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
